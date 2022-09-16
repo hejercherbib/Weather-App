fc8e034ec65ed141c20c722c5c32c70054b0a0df
Hejer Cherbib <hejercherbib@gmail.com>
2022-09-16 21:27:35 +0200

    initial commit - project structure

---
+++ app/src/main/java/com/cherbib/weatherapp/ui/mycities/MyCitiesActivity.kt
@@ -0,0 +1,12 @@
+package com.cherbib.weatherapp.ui.mycities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cherbib.weatherapp.R
+
+class MyCitiesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_cities)
+    }
+}
